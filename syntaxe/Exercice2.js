387104437107a95299facc81a3aa9ea35270a58c
Nasra <nasra@MacBook-Air-de-Nasra.local>
2025-06-03 14:56:59 +0200

    Ajout exercices 2 et 3

---
+++ syntaxe/Exercice2.js
@@ -0,0 +1,7 @@
+const prenom = "Nasra"
+const nom = "Ali"
+alert (`Bonjour ${prenom} ${nom}`)
+const retraite = 64
+let age = 28
+let result = retraite - age
+alert(`Il te reste ${result} années avant la retraite ! Courage ! En développant ce sera plus fun !`)
---
+++ syntaxe/Exercice3.js
@@ -0,0 +1,16 @@
+//Exercice 3
+
+// Que vaudront chacune des variables 
+// firstLanguage, secondLanguage  et thirdLanguage  à la fin du code suivant :
+let firstLanguage = "HTML"; --> Javascript
+let secondLanguage = "PHP"; --> Javascript
+let thirdLanguage = firstLanguage ; HTML
+firstLanguage  = "Javascript";
+secondLanguage = firstLanguage ;
+
+// Et dans ce cas-ci :
+let firstLanguage  = "HTML"; --> Javascript 
+let secondLanguage  = "PHP"; --> Javascript
+firstLanguage  = "Javascript"; ---> Javascript
+let thirdLanguage  = firstLanguage ;
+secondLanguage  = firstLanguage ;
