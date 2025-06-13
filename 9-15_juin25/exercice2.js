8a330e00f394368f6c9f49472636bc4d4e49f8e8
Nasra <nasra@MacBook-Air-de-Nasra.local>
2025-06-13 15:40:59 +0200

    Ajout exercice

--- 9-15_juin25/exercice2.js
+++ 9-15_juin25/exercice2.js
@@ -1,16 +1,40 @@
-// Exercice 1
+// // Exercice 1
+
+let x = prompt()
 function calculCarré(number){
     return number*number
 }
-console.log(calculCarré(3))
-console.log(calculCarré(2))
+// console.log(calculCarré(3))
+// console.log(calculCarré(2))
 
 // Exercice 2
 function calculCarré(number){
     return number*number
 }
-let resultCarré=calculCarré(3)
+let resultCarré=calculCarré(prompt)
 function multiplyNumber(y){  
 return y * 10
 }
+console.log(calculCarré(x))
 console.log(multiplyNumber(resultCarré))
+
+// Exercice 3
+
+let x = prompt()
+function calculCarré(number){
+    return number*number
+}
+// console.log(calculCarré(3))
+// console.log(calculCarré(2))
+
+// Exercice 2
+function calculCarré(number){
+    return number*number
+}
+let resultCarré=calculCarré(x)
+function multiplyNumber(y){  
+return y * 10 
+}
+alert (calculCarré(x))
+alert (multiplyNumber(resultCarré))
+
