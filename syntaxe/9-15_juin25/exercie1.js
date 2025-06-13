717acb20b7b0e8cbb68958f53f656fc82546689c
Nasra <nasra@MacBook-Air-de-Nasra.local>
2025-06-13 12:09:08 +0200

    Ajout exercice

---
+++ syntaxe/9-15_juin25/exercie1.js
@@ -0,0 +1,26 @@
+//Exercice 1
+
+function addition3Numbers(number1,number2,number3){
+    return number1+number2+number3
+    
+}
+console.log(`Le résultat de la somme est ${addition3Numbers(1,2,3)}`)
+
+//Exercice 2
+let a=8
+let b=3
+let c=4
+function addition3NumbersWithArg(a,b,c){
+let somme=a+b+c
+ console.log(`Le résulat de la somme est ${somme}`)
+}
+addition3NumbersWithArg(a,b,c)
+
+//Exercice 3
+function addition3NumbersWithArg(a,b,c){
+    let somme=a+b+c
+ console.log(`Le résulat de la somme est ${somme}`)
+}
+addition3NumbersWithArg(4,5,12)
+addition3NumbersWithArg(6,9,5)
+addition3NumbersWithArg(7,8,11)
